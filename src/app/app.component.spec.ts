434a1331055d570bec30d5028a3d37fab12edf17
Mickey Chu <binhminh88@Mickeys-Mac.local>
2017-08-18 01:10:06 +0300

    1st commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,73 @@
-import { TestBed, async } from '@angular/core/testing';
-
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  const TEXT_1 = 'A man, a plan, a canal, Panama!';
+  const TEXT_2 = 'Amor, Roma';
+  const TEXT_3 = 'race car';
+  const TEXT_4 = "No 'x' in Nixon";
+  const TEXT_5 = 'Was it a car or a cat I saw?';
+  const TEXT_6 = 'random';
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BrowserModule,
+        FormsModule
+      ]
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    component.ok_list = [];
+    component.fail_list = [];
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it('should check the palindrome correctly', () => {
+
+    expect(component.check_palindrome(TEXT_1)).toBe(true);
+    expect(component.check_palindrome(TEXT_2)).toBe(true);
+    expect(component.check_palindrome(TEXT_3)).toBe(true);
+    expect(component.check_palindrome(TEXT_4)).toBe(true);
+    expect(component.check_palindrome(TEXT_5)).toBe(true);
+    expect(component.check_palindrome(TEXT_6)).toBe(false);
+  });
+
+  it('should push the correct word into the ok_list without changes and in alphabetically order', () => {
+    component.check_palindrome(TEXT_3);
+    component.check_palindrome(TEXT_1);
+
+    expect(component.ok_list.length).toBe(2);
+    expect(component.ok_list[0]).toBe(TEXT_1);
+  });
+
+  it('should push the incorrect word into the fail_list invertedly', () => {
+
+    let inverted = TEXT_6.replace(/\W/g, '').toLowerCase().split('').reverse().join(''); 
+
+    component.check_palindrome(TEXT_6);
+
+    expect(component.fail_list.length).toBe(1);
+    expect(component.fail_list[0]).toBe(inverted);
+  });
+
+  /*it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
@@ -28,5 +78,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  }));*/
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	text 		: string;
+	ok_list 	: Array<string> = [];
+	fail_list 	: Array<string> = [];
+	regex 		: any = /[\W_]/g;
+
+	check_palindrome(text: string) {
+
+		try{
+			//if user enter some text and it's not an empty string then do the checking
+			if(text && text !== '') {
+
+				this.text = null;
+
+				//remove unwanted letter and lowercase all the letter
+				let word = text.replace(/\W/g, '').toLowerCase();
+
+				//reverse the word
+				let reverse_word = word.split('').reverse().join('');
+
+				//if the word and the reverse word match then the text is palidrome
+				if(word === reverse_word) {
+
+					//push the correct word to the list
+					this.ok_list.push(text);
+
+					//rearrange the list alphabetically
+					this.ok_list.sort();
+
+					return true;
+				}
+
+				//else it is not
+				//push the incorrect word to the list
+				this.fail_list.push(reverse_word);
+
+				//rearrange the list alphabetically
+				this.fail_list.sort();
+
+				return false;
+			}
+
+			//else warn the user to enter some text
+			alert('Please type something!')
+			throw new Error('Invalid text!');
+		}
+		catch (error) {
+			console.log(error)
+		}
+	}
 }
